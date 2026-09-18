c0c26f43f04914f408b8a1f83088c76be5d1ab4e
pretheevi <pretheeviraj0805@gmail.com>
2026-09-18 12:31:32 +0530

    deleted .env.example file for security purpose

--- connect.js
+++ connect.js
@@ -1,4 +1,27 @@
-const sqlite3 = require('sqlite3')
-const  {open} = require('sqlite')
+import sqlite3 from 'sqlite3';
+import {open} from 'sqlite';
 
 
+export async function connect() {
+    const db = await open({
+        filename: './database.db',
+        driver: sqlite3.Database
+    })
+    return db
+}
+
+class Database{
+
+    static async initializeTables() {
+        const NovaSectionTable = `
+            CREATE TABLE IF NOT EXISTS nova_sections(
+                id TEXT PRIMARY KEY,
+                section_name TEXT NOT NULL,
+            );
+        `
+    }
+
+    static async createAcknowledgmentRecords() {
+
+    }
+}
